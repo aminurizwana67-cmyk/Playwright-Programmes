76d8afe0394ac7638dbd8db36ad0ecddd42d3503
digital techonogies <digital techonogies@DESKTOP-HIH7PNF>
2025-12-16 11:45:50 +0530

    new change

--- src/main/java/com/mycompany/gitlearn/Gitlearn.java
+++ src/main/java/com/mycompany/gitlearn/Gitlearn.java
@@ -11,6 +11,6 @@ package com.mycompany.gitlearn;
 public class Gitlearn {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! java");
     }
 }
