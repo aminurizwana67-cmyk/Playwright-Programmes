ea01125e37ffa07e3f7d10455127f343e4c70b9d
digital techonogies <digital techonogies@DESKTOP-HIH7PNF>
2025-12-16 11:42:04 +0530

    new program

---
+++ src/main/java/com/mycompany/gitlearn/Gitlearn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitlearn;
+
+/**
+ *
+ * @author digital techonogies
+ */
+public class Gitlearn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
