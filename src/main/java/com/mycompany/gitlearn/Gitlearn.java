a231f0291f99986a453b518cb684f65c8c107f4b
aminurizwana67-cmyk <aminurizwana67@gmail.com>
2025-12-16 12:16:28 +0530

    Update Gitlearn.java

--- src/main/java/com/mycompany/gitlearn/Gitlearn.java
+++ src/main/java/com/mycompany/gitlearn/Gitlearn.java
@@ -11,6 +11,6 @@ package com.mycompany.gitlearn;
 public class Gitlearn {
 
     public static void main(String[] args) {
-        System.out.println("Hello ghio World! javajavapython");
+        System.out.println("Hello ghio World! welcometojavajavapython");
     }
 }
